74d0eeab33d9de01443ac6171fefaa4b1de30548
Marcus Lee <mokcandy97@gmail.com>
2022-05-17 04:42:18 -0600

    ADD VendingMachine

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,31 @@
+﻿VendingMachine v = new VendingMachine();
+v.Populate();
+v.getList();
+
+public class Item
+{
+    private string name;
+    private double price;
+    private string location;
+    private int quantity;
+
+    public Item(string name, double price, string location, int quantity = 1)
+    {
+        this.name = name;
+        this.price = price;
+        this.location = location;
+        this.quantity = quantity;
+    }
+
+    private double totalValue()
+    {
+        return quantity * price;
+    }
+
+    public void showItem()
+    {
+        Console.WriteLine($"{name} - {price} - {location} QTY:{quantity}");
+        Console.WriteLine($"TOTAL VALUE: {totalValue()}");
+        Console.WriteLine("--------------------------");
+    }
+}
---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,29 @@
+﻿public class VendingMachine
+{
+    private List<Item> items = new List<Item>();
+
+    public VendingMachine()
+    {
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("\nI'm a vending machine\n");
+        Console.WriteLine("--------------------------");
+    }
+
+    public void Populate()
+    {
+        items.Add(new Item("Cool Ranch Doritos", 2.5, "A1"));
+        items.Add(new Item("Reeses", 5.5, "A2"));
+        items.Add(new Item("Coke", 7.8, "A3"));
+
+    }
+
+    public void getList()
+    {
+        Console.WriteLine("Items in the vending machine: ");
+        Console.WriteLine("--------------------------");
+        foreach (Item item in items)
+        {
+            item.showItem();
+        }
+    }
+}
